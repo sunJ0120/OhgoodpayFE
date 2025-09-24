91a0c3a1dddb18c8cceabd30c7e64a8f3e9837a8
sunJ0120 <sspure1214@gmail.com>
2025-09-24 16:45:44 +0900

    [sunjung] fix: vite 부분에 프록시 처리 주석

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8090', // 스프링 부트 포트
-        changeOrigin: true,
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:8090', // 스프링 부트 포트
+    //     changeOrigin: true,
+    //   }
+    // },
   },
 })
